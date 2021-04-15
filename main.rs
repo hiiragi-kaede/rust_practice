66d7ce93a5714edd74ea0d59f47545e617832680
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-04-15 14:33:26 +0900

    参照について勉強

--- main.rs
+++ main.rs
@@ -1,9 +1,17 @@
 fn main(){
-    let num = 1;
-    let state = if num!=1{
-        false
-    }else{
-        true
-    };
-    println!("state is {}",state);
+    let s = String::from("hello");
+    takes_ownership(s);
+
+    let x = 5;
+    makes_copy(5);
+
+    println!("{}",x);
+}
+
+fn takes_ownership(some_str:String){
+    println!("{}",some_str);
+}
+
+fn makes_copy(some_int:i32){
+    println!("{}",some_int);
 }
