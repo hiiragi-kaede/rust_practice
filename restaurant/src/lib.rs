86f4a89c3258bec3bef5efa547b5ecf1776d4834
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-04-25 12:29:16 +0900

    useについて勉強

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -22,34 +22,35 @@ mod front_of_house{
 //     front_of_house::hosting::add_to_waitlist();
 // }
 
-fn serve_order(){}
+// fn serve_order(){}
 
-mod back_of_house{
-    pub struct Breakfast{
-        pub toast: String,
-        seasonal_fruit: String,
-    }
+// mod back_of_house{
+//     pub struct Breakfast{
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
 
-    impl Breakfast{
-        pub fn summer(toast: &str) -> Breakfast{
-            Breakfast{
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
+//     impl Breakfast{
+//         pub fn summer(toast: &str) -> Breakfast{
+//             Breakfast{
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
 
-    fn fix_incorrect_order(){
-        cook_order();
-        super::serve_order();
-    }
+//     fn fix_incorrect_order(){
+//         cook_order();
+//         super::serve_order();
+//     }
 
-    fn cook_order(){}
-}
+//     fn cook_order(){}
+// }
+
+use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant(){
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please",meal.toast);
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
