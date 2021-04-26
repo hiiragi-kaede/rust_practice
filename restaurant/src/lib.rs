b23eb4649f9ad916b0eb0b6f73cde938af241993
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-04-26 15:38:02 +0900

    モジュールのファイル分割について勉強

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,53 +1,6 @@
-mod front_of_house{
-    pub mod hosting{
-        pub fn add_to_waitlist(){}
+mod front_of_house;
 
-        fn seat_at_table(){}
-    }
-
-    mod serving{
-        fn take_order(){}
-
-        fn serve_order(){}
-
-        fn take_payment(){}
-    }
-}
-
-// pub fn eat_at_restaurant(){
-//     //絶対パス
-//     crate::front_of_house::hosting::add_to_waitlist();
-
-//     //相対パス
-//     front_of_house::hosting::add_to_waitlist();
-// }
-
-// fn serve_order(){}
-
-// mod back_of_house{
-//     pub struct Breakfast{
-//         pub toast: String,
-//         seasonal_fruit: String,
-//     }
-
-//     impl Breakfast{
-//         pub fn summer(toast: &str) -> Breakfast{
-//             Breakfast{
-//                 toast: String::from(toast),
-//                 seasonal_fruit: String::from("peaches"),
-//             }
-//         }
-//     }
-
-//     fn fix_incorrect_order(){
-//         cook_order();
-//         super::serve_order();
-//     }
-
-//     fn cook_order(){}
-// }
-
-use crate::front_of_house::hosting;
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant(){
     hosting::add_to_waitlist();
