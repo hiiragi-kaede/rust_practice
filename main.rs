c62ad3c622f4fec14defdd23490df1202714a773
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-04-30 11:57:34 +0900

    panic!について勉強

--- main.rs
+++ main.rs
@@ -1,14 +1,5 @@
 fn main() {
-use std::collections::HashMap;
+    let v = vec![1,2,3];
 
-let field_name = String::from("Favorite color");
-let field_value = String::from("Blue");
-
-let mut map = HashMap::new();
-map.insert(field_name, field_value);
-// field_name and field_value are invalid at this point, try using them and
-// see what compiler error you get!
-// field_nameとfield_valueはこの時点で無効になる。試しに使ってみて
-// どんなコンパイルエラーが出るか確認してみて！
-println!("{}",field_name);
+    v[99];
 }
