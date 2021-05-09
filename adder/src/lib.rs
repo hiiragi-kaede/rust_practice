afafbe97b219b14b4285e0e9e2b6052ac36adbc0
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-05-09 12:43:37 +0900

    テストのオプションについて勉強

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,13 +1,7 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+fn prints_and_returns_10(a: i32) -> i32 {
+    //{}という値を得た
+    println!("I got the value {}", a);
+    10
 }
 
 #[cfg(test)]
@@ -15,32 +9,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
     }
 
     #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
 
-fn main() {}
+fn main(){
+    
+}
