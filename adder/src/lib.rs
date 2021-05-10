2c3dd703fc98cb01e12ce35698c914f2828d097e
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-05-10 23:33:25 +0900

    テストについて勉強

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,7 +1,11 @@
-fn prints_and_returns_10(a: i32) -> i32 {
-    //{}という値を得た
-    println!("I got the value {}", a);
-    10
+
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -9,18 +13,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn this_test_will_pass() {
-        let value = prints_and_returns_10(4);
-        assert_eq!(10, value);
-    }
-
-    #[test]
-    fn this_test_will_fail() {
-        let value = prints_and_returns_10(8);
-        assert_eq!(5, value);
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
-
-fn main(){
-    
-}
+fn main(){}
